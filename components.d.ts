3f35e856ccca12dab300e3259a3e0a2fde39ef11
medmahdimaarouf <medmahdi.maarouf@gmail.com>
2025-07-09 00:02:40 +0100

    generate type definition components

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    Button: typeof import('primevue/button')['default']
     FloatingConfigurator: typeof import('./src/components/FloatingConfigurator.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
