bb2f038cfa1cbcfcb16b6f1fc892d54a100e8b2b
medmahdimaarouf <medmahdi.maarouf@gmail.com>
2025-07-22 08:26:08 +0100

    fixed direct flow

--- src/views/studio/flow/action/Action.vue
+++ src/views/studio/flow/action/Action.vue
@@ -1,14 +1,30 @@
 <script lang="ts" setup>
 import { PlaybookAction } from '@/models/playbook';
-import { Dimensions, Handle, Node, Position, useNode, useVueFlow } from '@vue-flow/core';
-import { defineProps, onMounted, reactive, watch } from 'vue';
+import { Dimensions, GraphNode, Handle, Node, Position, useNode, useVueFlow, XYPosition } from '@vue-flow/core';
+import { defineProps, onMounted, reactive } from 'vue';
 
 const { updateNode, addNodes, addEdges } = useVueFlow();
-const props = defineProps<{ node: PlaybookAction; placeholder: string }>();
-const placeholderId: String = reactive<String>(props.placeholder);
-const { node: vueFlowNode } = reactive(useNode());
+const props = defineProps<{ node: PlaybookAction; previousNode: string }>();
+const { node: previousNode } = useNode(props.previousNode);
+const { node: vueFlowNode } = useNode();
 const playbookAction: PlaybookAction = reactive<PlaybookAction>(props.node);
-const placeholdernNode = useNode(placeholderId.toString()).node;
+
+function getAbsolutePosition(node: GraphNode | undefined): XYPosition {
+    let x = 0;
+    let y = 0;
+    let current = node;
+
+    while (current) {
+        x += current.position.x;
+        y += current.position.y;
+        if (!current.parentNode) break;
+        current = useNode(current.parentNode).node;
+    }
+
+    return { x, y };
+}
+
+// Usage example in your component
 
 function drawNextPlaceholderNode(): Node {
     const actualDimensions: Dimensions = vueFlowNode.dimensions;
@@ -43,44 +59,25 @@ function drawNextPlaceholderNode(): Node {
 }
 
 function updatePositon() {
-    updateNode(
-        vueFlowNode.id,
-        vueFlowNode.parentNode
-            ? {
-                  position: {
-                      x: placeholdernNode.position.x + placeholdernNode.dimensions.width / 2 - vueFlowNode.dimensions.width / 2 - 10,
-                      y: placeholdernNode.position.y + 30
-                  }
-              }
-            : {
-                  position: {
-                      x: placeholdernNode.position.x + placeholdernNode.dimensions.width / 2 - vueFlowNode.dimensions.width / 2 - 10,
-                      y: placeholdernNode.position.y + 30
-                  }
-              }
-    );
-}
-function attachToPlaceHolder() {
-    console.log(placeholderId);
-    updatePositon();
-    addEdges([
-        {
-            id: `${placeholdernNode.id}|${vueFlowNode.id}`,
-            source: placeholdernNode.id,
-            target: vueFlowNode.id,
-            sourceHandle: `${vueFlowNode.id}-next`,
-            targetHandle: `previous`
+    const previousNodeDimensions: Dimensions = previousNode.dimensions;
+    const previousNodePositon: XYPosition = getAbsolutePosition(previousNode);
+    const actualDimensions: Dimensions = {
+        width: useNode(previousNode.id).nodeEl.value?.getBoundingClientRect().width!,
+        height: useNode(previousNode.id).nodeEl.value?.getBoundingClientRect().height!
+    };
+    let x = previousNodePositon.x;
+    x += previousNodeDimensions.width / 2;
+    x -= actualDimensions.width / 2;
+    updateNode(vueFlowNode.id, {
+        position: {
+            x: x,
+            y: previousNodePositon.y + previousNodeDimensions.height + 30
         }
-    ]);
+    });
 }
+
 onMounted(() => {
-    attachToPlaceHolder();
-    watch(
-        () => vueFlowNode.dimensions,
-        (d) => {
-            updatePositon();
-        }
-    );
+    updatePositon();
     setTimeout(() => {
         drawNextPlaceholderNode();
     }, 0);
@@ -88,13 +85,20 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="flex flex-center content-center items-center gap-2 cursor-pointer px-4 py-2">
+    <div class="flex flex-center content-center items-center gap-2 cursor-pointer px-4 my-4 bg-white rounded">
         <Avatar icon="pi pi-sitemap" size="large" />
         <div>
-            <span>{{ $props.node.name }}</span>
+            <span class="text-xl font-bold m-0">{{ $props.node.name }}</span>
             <p>{{ $props.node.selector }}</p>
         </div>
     </div>
+
+    <!--
+        <div class="next-container flex flex-center content-center items-center gap-2 px-4 py-2">
+            <Button icon="pi pi-plus-circle" label="Add next"></Button>
+        </div>
+    -->
+
     <Handle
         id="previous"
         type="target"
@@ -106,17 +110,7 @@ onMounted(() => {
             height: '6px'
         }"
     />
-    <Handle
-        id="next"
-        type="source"
-        :position="Position.Bottom"
-        :style="{
-            backgroundColor: 'lime',
-            borderRadius: '0%',
-            width: '6px',
-            height: '6px'
-        }"
-    />
+    <Handle id="next" type="source" :position="Position.Bottom" />
     <Handle
         id="pipe"
         type="source"
